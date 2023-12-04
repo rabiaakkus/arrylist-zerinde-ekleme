f0cf31cd48333910b48cfb7cda71ca57da0c2cce
rabiaakkus <145451092+rabiaakkus@users.noreply.github.com>
2023-12-04 12:07:56 +0300

    Add files via upload

---
+++ arrylist uzerinde ekleme/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace arrylist_uzerinde_ekleme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSehirler = new System.Windows.Forms.Label();
+            this.txtSehirler = new System.Windows.Forms.TextBox();
+            this.lblDurum = new System.Windows.Forms.Label();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnAraEkle = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnAra = new System.Windows.Forms.Button();
+            this.lbListeSehirler = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lblSehirler
+            // 
+            this.lblSehirler.AutoSize = true;
+            this.lblSehirler.Location = new System.Drawing.Point(21, 27);
+            this.lblSehirler.Name = "lblSehirler";
+            this.lblSehirler.Size = new System.Drawing.Size(42, 13);
+            this.lblSehirler.TabIndex = 0;
+            this.lblSehirler.Text = "Şehirler";
+            // 
+            // txtSehirler
+            // 
+            this.txtSehirler.Location = new System.Drawing.Point(62, 27);
+            this.txtSehirler.Name = "txtSehirler";
+            this.txtSehirler.Size = new System.Drawing.Size(100, 20);
+            this.txtSehirler.TabIndex = 1;
+            // 
+            // lblDurum
+            // 
+            this.lblDurum.AutoSize = true;
+            this.lblDurum.Location = new System.Drawing.Point(21, 163);
+            this.lblDurum.Name = "lblDurum";
+            this.lblDurum.Size = new System.Drawing.Size(48, 13);
+            this.lblDurum.TabIndex = 2;
+            this.lblDurum.Text = "DURUM";
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(12, 93);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(75, 23);
+            this.btnEkle.TabIndex = 3;
+            this.btnEkle.Text = "EKLE";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnAraEkle
+            // 
+            this.btnAraEkle.Location = new System.Drawing.Point(104, 93);
+            this.btnAraEkle.Name = "btnAraEkle";
+            this.btnAraEkle.Size = new System.Drawing.Size(92, 23);
+            this.btnAraEkle.TabIndex = 4;
+            this.btnAraEkle.Text = "ARAYA EKLE";
+            this.btnAraEkle.UseVisualStyleBackColor = true;
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Location = new System.Drawing.Point(202, 93);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(75, 23);
+            this.btnGuncelle.TabIndex = 5;
+            this.btnGuncelle.Text = "GÜNCELLE";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(304, 93);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(75, 23);
+            this.btnSil.TabIndex = 6;
+            this.btnSil.Text = "SİL";
+            this.btnSil.UseVisualStyleBackColor = true;
+            // 
+            // btnAra
+            // 
+            this.btnAra.Location = new System.Drawing.Point(397, 93);
+            this.btnAra.Name = "btnAra";
+            this.btnAra.Size = new System.Drawing.Size(75, 23);
+            this.btnAra.TabIndex = 7;
+            this.btnAra.Text = "ARA";
+            this.btnAra.UseVisualStyleBackColor = true;
+            // 
+            // lbListeSehirler
+            // 
+            this.lbListeSehirler.FormattingEnabled = true;
+            this.lbListeSehirler.Location = new System.Drawing.Point(79, 163);
+            this.lbListeSehirler.Name = "lbListeSehirler";
+            this.lbListeSehirler.Size = new System.Drawing.Size(117, 147);
+            this.lbListeSehirler.TabIndex = 8;
+            this.lbListeSehirler.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(486, 332);
+            this.Controls.Add(this.lbListeSehirler);
+            this.Controls.Add(this.btnAra);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.btnAraEkle);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.lblDurum);
+            this.Controls.Add(this.txtSehirler);
+            this.Controls.Add(this.lblSehirler);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSehirler;
+        private System.Windows.Forms.TextBox txtSehirler;
+        private System.Windows.Forms.Label lblDurum;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnAraEkle;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnAra;
+        private System.Windows.Forms.ListBox lbListeSehirler;
+    }
+}
+
